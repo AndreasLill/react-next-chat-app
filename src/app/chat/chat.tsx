5edf36999e954298d17d6230107ab3a1b72a1fb1
AndreasLill <andreas.ste.lill@gmail.com>
2023-06-19 18:01:41 +0200

    Added chat client side socket events and listeners.

--- src/app/chat/chat.tsx
+++ src/app/chat/chat.tsx
@@ -11,14 +11,23 @@ import DialogRoomCreate from './dialog/dialog-room-create'
 import ButtonIcon from '@/components/button-icon'
 import DialogRoomJoin from './dialog/dialog-room-join'
 import Tooltip from '@/components/tooltip'
+import Pusher from 'pusher-js'
+import { Options } from 'pusher-js'
+import { Message } from '@/types/message'
 
 const fetcher = (url: string) => fetch(url, { method: 'GET' }).then((res: Response) => res.json())
+const pusher = new Pusher(
+    process.env.NEXT_PUBLIC_PUSHER_KEY as string,
+    {
+        cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER
+    } as Options
+)
 
 export default function Chat() {
     const { data: user } = useSWR<User>('/api/user/get', fetcher)
-    const [activeRoom, setActiveRoom] = useState<string>('No Room Selected')
-    const [roomStatus, setRoomStatus] = useState<string>('Disconnected')
-    const [message, setMessage] = useState<string>('')
+    const [activeRoom, setActiveRoom] = useState<string>('')
+    const [messages, setMessages] = useState<Message[]>([])
+    const [input, setInput] = useState<string>('')
     const [dialogCreateRoom, setDialogCreateRoom] = useState<boolean>(false)
     const [dialogJoinRoom, setDialogJoinRoom] = useState<boolean>(false)
 
@@ -27,12 +36,12 @@ export default function Chat() {
             method: 'POST',
             body: JSON.stringify({ name: name })
         }).then((res: Response) => res)
-
+        const id = await response.json()
         console.log(response)
         // Revalidate cache to fetch new data.
         // TODO: Change later to mutate data in cache.
         mutate('/api/user/get').then(() => {
-            console.log('Room was created.')
+            console.log(`Room ${id} was created.`)
         })
     }
 
@@ -46,9 +55,31 @@ export default function Chat() {
         // Revalidate cache to fetch new data.
         // TODO: Change later to mutate data in cache.
         mutate('/api/user/get').then(() => {
-            setActiveRoom(id)
-            console.log('Joined the room.')
+            console.log(`Joined room ${id}.`)
+            onConnectToRoom(id)
         })
+    }
+
+    async function onConnectToRoom(id: string) {
+        if (activeRoom) {
+            // Disconnect the current active room.
+            pusher.unsubscribe(activeRoom)
+            console.log(`Pusher unsubscribed from room ${activeRoom}.`)
+
+            if (activeRoom === id) {
+                // The new room is the same as previous room, so just return.
+                setActiveRoom('')
+                return
+            }
+        }
+        const channel = pusher.subscribe(id)
+        channel.bind('message', onReceiveMessage)
+        console.log(`Pusher subscribed to room ${id}.`)
+        setActiveRoom(id)
+    }
+
+    async function onReceiveMessage(data: any) {
+        console.log(`Pusher: ${data}`)
     }
 
     useEffect(() => {
@@ -56,7 +87,7 @@ export default function Chat() {
     }, [user])
 
     return (
-        <div className="flex mx-auto max-w-7xl h-screen px-8 py-24 space-x-4">
+        <div className="flex mx-auto max-w-[96rem] h-screen px-8 py-24 space-x-4">
             <div className="flex flex-col w-80 h-full space-y-4">
                 <div className="flex justify-between items-center p-6 bg-white dark:bg-zinc-900 rounded-lg shadow">
                     <div className="flex items-center">
@@ -67,7 +98,7 @@ export default function Chat() {
                         <ButtonIcon icon={<LogOut size={24} />} onClick={() => signOut()} />
                     </Tooltip>
                 </div>
-                <div className="flex flex-grow flex-col p-6 bg-white dark:bg-zinc-900 rounded-lg shadow space-y-6 overflow-hidden">
+                <div className="flex flex-1 flex-col p-6 bg-white dark:bg-zinc-900 rounded-lg shadow space-y-6 overflow-hidden">
                     <div className="flex space-x-2">
                         <Tooltip text="Create Room">
                             <ButtonIcon icon={<Plus size={24} />} onClick={() => setDialogCreateRoom(true)} />
@@ -88,37 +119,36 @@ export default function Chat() {
                                 key={room.id}
                                 text={room.name}
                                 toggled={room.id === activeRoom}
-                                onClick={() => setActiveRoom(room.id)}
+                                onClick={() => onConnectToRoom(room.id)}
                             />
                         ))}
                     </ul>
                 </div>
             </div>
-            <div className="flex flex-col justify-between flex-grow h-full bg-white dark:bg-zinc-900 rounded-lg shadow">
-                <div className="flex justify-between p-6">
-                    <div className="flex flex-col justify-center">
-                        <p className="text-sm">{activeRoom}</p>
-                        <p className="text-sm">{roomStatus}</p>
-                    </div>
+            <div className="flex flex-col flex-1 h-full bg-white dark:bg-zinc-900 rounded-lg shadow overflow-auto">
+                <div className="flex items-center justify-between p-6">
+                    <p className="text-sm">{activeRoom}</p>
                     <div className="flex">
                         <Tooltip text="Options">
                             <ButtonIcon icon={<MoreVertical />} onClick={() => {}} />
                         </Tooltip>
                     </div>
                 </div>
-                <div className="flex flex-grow flex-col space-y-6 p-6 mx-6 overflow-scroll">
-                    <p>MESSAGE</p>
-                    <p>MESSAGE</p>
-                    <p>MESSAGE</p>
+                <div className="flex flex-1 flex-col mx-6 bg-slate-100 dark:bg-zinc-950 border rounded-lg border-black/20 dark:border-white/20 overflow-y-scroll">
+                    {messages?.map((message) => (
+                        <p className="pt-6 px-6 break-all" key={message.id}>
+                            {`${message.user}: ${message.text}`}
+                        </p>
+                    ))}
                 </div>
-                <div className="p-8">
+                <div className="p-6">
                     <Input
                         id="chat"
                         type="text"
                         placeholder="Message To Chat"
                         className="w-full py-4 px-4"
-                        value={message}
-                        onChange={(e) => setMessage(e)}
+                        value={input}
+                        onChange={(e) => setInput(e)}
                     />
                 </div>
             </div>
---
+++ src/types/message.ts
@@ -0,0 +1,7 @@
+export type Message = {
+    id: string
+    sent: string
+    room: string
+    user: string
+    text: string
+}
