c4c0ab25302c1baf0ca1c7678dc8bde97d995841
AndreasLill <andreas.ste.lill@gmail.com>
2023-06-27 23:20:58 +0200

    Renamed button styles.

--- src/ui/button/button.tsx
+++ src/ui/button/button.tsx
@@ -10,13 +10,13 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const styles = {
-    filledDefault: clsx(
+    filled: clsx(
         'rounded-md border border-transparent text-sm font-semibold outline-none focus-visible:outline-primary text-on-primary bg-primary hover:bg-primary/80 active:translate-y-[1px] transition-colors'
     ),
-    outlineDefault: clsx(
+    outline: clsx(
         'rounded-md border text-sm font-semibold outline-none focus-visible:outline-primary border-primary text-primary hover:bg-primary/10 active:translate-y-[1px] transition-colors'
     ),
-    subtleDefault: clsx(
+    subtle: clsx(
         'rounded-md border border-transparent text-sm font-semibold outline-none focus-visible:outline-primary text-primary hover:bg-primary/10 active:translate-y-[1px] transition-colors'
     ),
     disabled: clsx(
@@ -34,9 +34,9 @@ const Button = forwardRef<HTMLButtonElement, Props>(({ variant, icon, text, load
             {...props}
             ref={forwardedRef}
             className={clsx(
-                variant === 'filled' && (props.disabled ? styles.disabled : styles.filledDefault),
-                variant === 'outline' && (props.disabled ? styles.disabled : styles.outlineDefault),
-                variant === 'subtle' && (props.disabled ? styles.disabled : styles.subtleDefault),
+                variant === 'filled' && (props.disabled ? styles.disabled : styles.filled),
+                variant === 'outline' && (props.disabled ? styles.disabled : styles.outline),
+                variant === 'subtle' && (props.disabled ? styles.disabled : styles.subtle),
                 loading && 'cursor-default opacity-50',
                 text ? 'px-3 py-2' : 'p-2',
                 className
